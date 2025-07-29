7c8ca645b539570a9447ff12155c6a5664331e13
Zulfiana Rahmi <zulfianarahmi4@gmail.com>
2025-07-29 19:56:18 +0700

    Update Next.js configuration to enable static export, enforce trailing slashes, and adjust image optimization settings. Configure ESLint and TypeScript to ignore build errors for improved development workflow.

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  trailingSlash: true,
+  images: {
+    unoptimized: true
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  }
 };
 
 export default nextConfig;
